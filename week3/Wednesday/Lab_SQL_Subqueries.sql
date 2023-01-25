65ee4980189b24be44b202341dcc7404b005361b
ajaykrishna2810 <47384786+ajaykrishna2810@users.noreply.github.com>
2023-01-25 16:30:37 +0100

    did the 6th optional on sub queries

--- week3/Wednesday/Lab_SQL_Subqueries.sql
+++ week3/Wednesday/Lab_SQL_Subqueries.sql
@@ -22,7 +22,7 @@ where actor_id in (select actor_id from film_actor
  ##    Note that to create a join, you will have to identify the correct tables with their primary keys and foreign keys, that will help you get the relevant information.
 
 # using subqueries
-select email from customer
+select concat(customer.first_name, '', customer.last_name) 'Name', '', email 'Email' from customer
 where address_id in (select address_id from address
        where city_id in (select city_id from city
                where country_id in (select country_id from country where country like "Canada")));
@@ -34,4 +34,13 @@ join city on address.city_id = city.city_id
 join country on city.country_id = country.country_id
 where country.country like "Canada"; 
   
-                          
+## 6. Which are films starred by the most prolific actor? Most prolific actor is defined as the actor that has acted in the most number of films. 
+##    First you will have to find the most prolific actor and then use that actor_id to find the different films that he/she starred.  
+select title FROM film
+WHERE film_id in (SELECT film_id FROM film_actor
+WHERE actor_id LIKE (select actor_id from actor
+join film_actor
+using(actor_id)
+group by actor.actor_id
+order by count(actor.actor_id) desc limit 1));
+                
